264b9a62928ffdbf09cf4787aa8c11a568f431cf
Sewon Kim <sewonkim0black@gmail.com>
2023-05-14 19:47:49 -0400

    pre deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      //
+      <video controls width="640" height="360">
+        <source src="winter-snow.mp4" type="video/mp4" />
+        Error: Cannot load video.
+      </video>
     </div>
   );
 }
